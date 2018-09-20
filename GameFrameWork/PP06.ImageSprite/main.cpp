c8a38f909bfdca87883a52b6284b81f79ebd5677
김수민 <kimsm23236@naver.com>
2018-09-20 17:56:18 +0900

    9월 20일 PP06.ImageSprite

---
+++ GameFrameWork/PP06.ImageSprite/main.cpp
@@ -0,0 +1,20 @@
+#include "Game.h"
+
+Game* g_game = 0;	// our Game object
+
+int main(int argc, char* argv[])
+{
+	g_game = new Game();
+	g_game->init("PP05.AnimatingSprite", 100, 100, 640, 480, false);
+
+	while (g_game->running())
+	{
+		g_game->handleEvents();
+		g_game->update();
+		g_game->render();
+	}
+	SDL_Delay(3000);
+	g_game->clean();
+	SDL_Delay(3000);
+	return 0;
+}
