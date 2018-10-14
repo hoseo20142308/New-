dd462f4ad9e4473a28d5af844f613c599ea0c67b
김수민 <kimsm23236@naver.com>
2018-10-15 01:57:13 +0900

    123

---
+++ MiniProject/Game.h
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "SDL.h"
+
+class Game
+{
+public:
+	Game() {}
+	~Game() {}
+
+	bool init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
+	void render();
+	void update();
+	void handleEvents();
+	void clean();
+	bool running() { return m_bRunning; }
+private:
+	SDL_Window * m_pWindow;
+	SDL_Renderer* m_pRenderer;
+	SDL_Texture* m_pTexture;			// the new SDL_Texture variable
+	SDL_Rect m_sourceRectangle;			// 원본 사각형
+	SDL_Rect m_destinationRectangle;	// 대상 사각형
+	bool m_bRunning;
+};
