49cdeb328f8e50965a3573b18b70769a7d545444
김수민 <kimsm23236@naver.com>
2018-10-28 19:39:49 +0900

    PP12 PP11 복사 커밋

---
+++ PP12.Vector2D/Player.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "SDLGameObject.h"
+
+class Player : public SDLGameObject
+{
+public:
+	Player(const LoaderParams* p_Params);
+	virtual void draw();
+	virtual void update();
+	virtual void clean() {};
+};
